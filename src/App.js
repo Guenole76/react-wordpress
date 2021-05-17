9913dae5edf6bdd8ccd939dad7451d60ef97e9a8
Guenole76 <54894682+Guenole76@users.noreply.github.com>
2021-05-18 00:10:18 +0200

    react-wp

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import vita from './vita.png';
+import rhoba from './rhoba.jpg';
+import React from 'react';
+import './App.css';
+
+import { BrowserRouter , Route} from 'react-router-dom';
+import Accueil from './components/Accueil';
+import Event from './components/Event';
+import Menu from './components/Menu';
+import Allumnis from './components/Allumnis';
+
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Route exact path="/" component ={Accueil} />
+      <Route path="/event" component ={Event} />
+      <Route strict path="/allumnis" component ={Allumnis} />
+    </BrowserRouter>
+  )
+
+}
+export default App;
---
+++ src/components/Accueil.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import conf from './conf.jpg';
+import vita from './vita.png';
+import rhoba from './rhoba.jpg';
+const Accueil = () => {
+    return (
+    <div className="app2">
+        <div className="pres">
+            <div className="text5">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+                Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+                packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
+                volved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+                Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+                packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
+                volved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+                Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+                packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
+                volved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+                Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+                packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
+                
+              </p>
+            </div>
+            <img src={conf} id="conf" alt="conf" />
+        </div>
+
+
+        <div className="bas2">
+            
+        </div>
+        
+    </div>
+    )
+
+
+}
+
+
+export default Accueil;
